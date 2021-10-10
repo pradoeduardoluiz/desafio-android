691f5166327a18bb6cc623f382d332d251181ad0
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 12:41:35 -0300

    feat: change users fragment and add default struct for using mvi

--- app/src/main/java/com/picpay/desafio/android/ui/UsersFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/UsersFragment.kt
@@ -1,7 +1,38 @@
 package com.picpay.desafio.android.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.databinding.UsersFragmentBinding
+import com.picpay.desafio.android.shared.android.extensions.viewBinding
+import com.picpay.desafio.android.shared.android.extensions.watch
 
-class UsersFragment : Fragment() {
+class UsersFragment : Fragment(R.layout.users_fragment) {
+
+  private val binding by viewBinding(UsersFragmentBinding::bind)
+  private val viewModel: UsersContract.ViewModel by viewModels<UsersViewModel>()
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    bindRecyclerView()
+    bindInputs()
+    bindOutputs()
+  }
+
+  private fun bindInputs() = with(binding) {
+
+  }
+
+  private fun bindOutputs() {
+    watch(viewModel.state) { state ->
+
+    }
+  }
+
+  private fun bindRecyclerView() {
+
+  }
 
 }
