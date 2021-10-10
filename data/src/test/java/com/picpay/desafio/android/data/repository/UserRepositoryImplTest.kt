416cbcc36cf6ede2a8f40dc9ca518120438a48ed
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 10:27:19 -0300

    feat: create user repository unit test

--- data/src/test/java/com/picpay/desafio/android/data/repository/UserRepositoryImplTest.kt
+++ data/src/test/java/com/picpay/desafio/android/data/repository/UserRepositoryImplTest.kt
@@ -2,15 +2,15 @@ package com.picpay.desafio.android.data.repository
 
 import com.picpay.desafio.android.data.UserMapper
 import com.picpay.desafio.android.data.source.cache.dao.UserDAO
+import com.picpay.desafio.android.data.source.cache.dbo.UserDBO
+import com.picpay.desafio.android.data.source.remote.dto.UserDTO
 import com.picpay.desafio.android.data.source.remote.service.PicPayService
 import com.picpay.desafio.android.domain.models.UserModel
 import com.picpay.desafio.android.domain.repository.UserRepository
 import com.picpay.desafio.android.shared.kotlin.GetCurrentTime
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.confirmVerified
-import io.mockk.mockk
+import io.mockk.*
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
@@ -24,6 +24,8 @@ class UserRepositoryImplTest {
 
     private lateinit var userRepository: UserRepository
 
+    private val currentTimeMock = 1000000000000
+
     @Before
     fun `setup test`() {
         userRepository = UserRepositoryImpl(
@@ -35,24 +37,163 @@ class UserRepositoryImplTest {
     }
 
     @Test
-    fun `should get users and save in local cached`() = runBlocking {
-        val expected = listOf(
-            UserModel(
-                id = "id",
-                name = "name",
-                imageUrl = "imageUrl",
-                username = "username"
-            )
+    fun `should get users from api and save in local cache`() = runBlocking {
+        val model = UserModel(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username"
         )
 
+        val expected = listOf(model)
+
+        val dto = UserDTO(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username"
+        )
+        val dtos = listOf(dto)
+
+        val dbo = UserDBO(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username",
+            updateDate = currentTimeMock
+        )
+        val dbos = listOf(dbo)
+
+        val usersFlow = flowOf(dbos)
+
+        coEvery { getCurrentTime() } returns currentTimeMock
         coEvery { dao.getOldestRegisterUpdated() } returns null
+        coEvery { service.getUsers() } returns dtos
+        coEvery { dao.deleteAll() } just runs
+        coEvery { dao.insert(dbos) } returns listOf(0)
+        coEvery { mapper.mapToDbo(dto, currentTimeMock) } returns dbo
+        coEvery { dao.getUsers() } returns usersFlow
+        coEvery { mapper.mapToModel(dbo) } returns model
 
         val actualFlow = userRepository.getUsers()
         actualFlow.collect { actual ->
             Assert.assertEquals(expected, actual)
         }
 
-        coVerify(exactly = 1) { dao.getOldestRegisterUpdated() }
+        coVerify(exactly = 1) {
+            getCurrentTime()
+            dao.getOldestRegisterUpdated()
+            service.getUsers()
+            dao.deleteAll()
+            dao.insert(dbos)
+            mapper.mapToDbo(dto, currentTimeMock)
+            dao.getUsers()
+            mapper.mapToModel(dbo)
+        }
+
+        confirmVerified(service, dao, mapper, getCurrentTime)
+    }
+
+    @Test
+    fun `should get users from local cache`() = runBlocking {
+        val model = UserModel(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username"
+        )
+
+        val expected = listOf(model)
+
+        val dbo = UserDBO(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username",
+            updateDate = currentTimeMock
+        )
+        val dbos = listOf(dbo)
+
+        val usersFlow = flowOf(dbos)
+
+        val oldestUpdatedRegister = 999999970001
+
+        coEvery { getCurrentTime() } returns currentTimeMock
+        coEvery { dao.getOldestRegisterUpdated() } returns oldestUpdatedRegister
+        coEvery { dao.getUsers() } returns usersFlow
+        coEvery { mapper.mapToModel(dbo) } returns model
+
+        val actualFlow = userRepository.getUsers()
+        actualFlow.collect { actual ->
+            Assert.assertEquals(expected, actual)
+        }
+
+        coVerify(exactly = 1) {
+            getCurrentTime()
+            dao.getOldestRegisterUpdated()
+            dao.getUsers()
+            mapper.mapToModel(dbo)
+        }
+
+        confirmVerified(service, dao, mapper, getCurrentTime)
+    }
+
+    @Test
+    fun `should check cache validity and update local cache`() = runBlocking {
+        val model = UserModel(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username"
+        )
+
+        val expected = listOf(model)
+
+        val dto = UserDTO(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username"
+        )
+        val dtos = listOf(dto)
+
+        val dbo = UserDBO(
+            id = "id",
+            name = "name",
+            imageUrl = "imageUrl",
+            username = "username",
+            updateDate = currentTimeMock
+        )
+        val dbos = listOf(dbo)
+
+        val usersFlow = flowOf(dbos)
+
+        val oldestUpdatedRegister = 999999970000
+
+        coEvery { getCurrentTime() } returns currentTimeMock
+        coEvery { dao.getOldestRegisterUpdated() } returns oldestUpdatedRegister
+        coEvery { service.getUsers() } returns dtos
+        coEvery { dao.deleteAll() } just runs
+        coEvery { dao.insert(dbos) } returns listOf(0)
+        coEvery { mapper.mapToDbo(dto, currentTimeMock) } returns dbo
+        coEvery { dao.getUsers() } returns usersFlow
+        coEvery { mapper.mapToModel(dbo) } returns model
+
+        val actualFlow = userRepository.getUsers()
+        actualFlow.collect { actual ->
+            Assert.assertEquals(expected, actual)
+        }
+
+        coVerify(exactly = 1) {
+            getCurrentTime()
+            dao.getOldestRegisterUpdated()
+            service.getUsers()
+            dao.deleteAll()
+            dao.insert(dbos)
+            mapper.mapToDbo(dto, currentTimeMock)
+            dao.getUsers()
+            mapper.mapToModel(dbo)
+        }
 
         confirmVerified(service, dao, mapper, getCurrentTime)
     }
