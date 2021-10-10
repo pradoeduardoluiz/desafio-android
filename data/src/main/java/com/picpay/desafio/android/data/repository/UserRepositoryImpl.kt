34d47d57cb4dc39633734c92eaa709aa7fa66f46
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 13:52:05 -0300

    feat: changed cache timeout value

--- data/src/main/java/com/picpay/desafio/android/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/picpay/desafio/android/data/repository/UserRepositoryImpl.kt
@@ -10,28 +10,28 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class UserRepositoryImpl(
-    private val service: PicPayService,
-    private val dao: UserDAO,
-    private val mapper: UserMapper,
-    private val getCurrentTime: GetCurrentTime
+  private val service: PicPayService,
+  private val dao: UserDAO,
+  private val mapper: UserMapper,
+  private val getCurrentTime: GetCurrentTime
 ) : UserRepository {
 
-    override suspend fun getUsers(): Flow<List<UserModel>> {
-        val currentTime = getCurrentTime()
-        val oldestUpdated = dao.getOldestRegisterUpdated() ?: 0
+  override suspend fun getUsers(): Flow<List<UserModel>> {
+    val currentTime = getCurrentTime()
+    val oldestUpdated = dao.getOldestRegisterUpdated() ?: 0
 
-        val hasNeedUpdateCache = currentTime - oldestUpdated >= CACHE_TIMEOUT
-        if (hasNeedUpdateCache) {
-            val response = service.getUsers()
-            val users = response.map { user -> mapper.mapToDbo(user, updateDate = currentTime) }
-            dao.deleteAll()
-            dao.insert(users)
-        }
-
-        return dao.getUsers().map { users -> users.map(mapper::mapToModel) }
+    val hasNeedUpdateCache = currentTime - oldestUpdated >= CACHE_TIMEOUT
+    if (hasNeedUpdateCache) {
+      val response = service.getUsers()
+      val users = response.map { user -> mapper.mapToDbo(user, updateDate = currentTime) }
+      dao.deleteAll()
+      dao.insert(users)
     }
 
-    private companion object {
-        const val CACHE_TIMEOUT = 30000
-    }
+    return dao.getUsers().map { users -> users.map(mapper::mapToModel) }
+  }
+
+  private companion object {
+    const val CACHE_TIMEOUT = 300000
+  }
 }
