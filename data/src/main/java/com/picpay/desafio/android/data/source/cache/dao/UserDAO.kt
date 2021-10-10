8ee1d7e67c25cf4c04a2ef97fb08145e465ee283
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 13:57:31 -0300

    feat: change order by from name to id

--- data/src/main/java/com/picpay/desafio/android/data/source/cache/dao/UserDAO.kt
+++ data/src/main/java/com/picpay/desafio/android/data/source/cache/dao/UserDAO.kt
@@ -8,12 +8,12 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 abstract class UserDAO : BaseDAO<UserDBO>() {
 
-    @Query(value = "SELECT * FROM user ORDER BY name ASC")
-    abstract fun getUsers(): Flow<List<UserDBO>>
+  @Query(value = "SELECT * FROM user ORDER BY id ASC")
+  abstract fun getUsers(): Flow<List<UserDBO>>
 
-    @Query(value = "SELECT updateDate FROM user ORDER BY updateDate ASC")
-    abstract fun getOldestRegisterUpdated(): Long?
+  @Query(value = "SELECT updateDate FROM user ORDER BY updateDate ASC")
+  abstract fun getOldestRegisterUpdated(): Long?
 
-    @Query(value = "DELETE FROM user")
-    abstract fun deleteAll()
+  @Query(value = "DELETE FROM user")
+  abstract fun deleteAll()
 }
