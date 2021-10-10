20507b9ed9d868b3411127f76463466fbbe996e3
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 12:28:39 -0300

    feat: create navigation and setup navigation on main activity

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.picpay.desafio.android
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
 import com.picpay.desafio.android.databinding.ActivityMainBinding
+import com.picpay.desafio.android.navigation.Navigator
 import com.picpay.desafio.android.shared.android.extensions.viewBinding
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
   private val binding by viewBinding(ActivityMainBinding::inflate)
+
+  @Inject
+  lateinit var navigator: Navigator
+
+  override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    super.onCreate(savedInstanceState, persistentState)
+    setupNavigation()
+  }
+
+  private fun setupNavigation() {
+    val navHostFragment = supportFragmentManager
+      .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    navigator.bind(navHostFragment.navController, lifecycleScope)
+  }
 
 }
---
+++ app/src/main/java/com/picpay/desafio/android/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.picpay.desafio.android.di
+
+import com.picpay.desafio.android.navigation.Navigator
+import com.picpay.desafio.android.navigation.NavigatorImpl
+import com.picpay.desafio.android.shared.android.dispatchers.AppDispatchersProvider
+import com.picpay.desafio.android.shared.android.dispatchers.DispatchersProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+  @Provides
+  @Singleton
+  fun providerDispatchersProvider(): DispatchersProvider = AppDispatchersProvider()
+
+  @Provides
+  @Singleton
+  fun providerNavigator(
+    dispatchersProvider: DispatchersProvider
+  ): Navigator = NavigatorImpl(dispatchersProvider)
+
+}
