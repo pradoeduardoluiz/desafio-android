79b3591beefabe6961b0fb53011769cec512edde
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 11:21:47 -0300

    feat: load binding usign view binding on main activity

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.picpay.desafio.android
 
 import androidx.appcompat.app.AppCompatActivity
+import com.picpay.desafio.android.databinding.ActivityMainBinding
+import com.picpay.desafio.android.shared.android.extensions.viewBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+  private val binding by viewBinding(ActivityMainBinding::inflate)
 
 }
