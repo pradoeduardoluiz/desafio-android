273f295d85fc9d5e353b974258f7c5e7bf3f6308
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-10 13:16:27 -0300

    feat: create user mapper

---
+++ app/src/main/java/com/picpay/desafio/android/ui/users/UserMapper.kt
@@ -0,0 +1,12 @@
+package com.picpay.desafio.android.ui.users
+
+import com.picpay.desafio.android.domain.models.UserModel
+
+class UserMapper {
+  fun mapToState(user: UserModel) = UsersState.User(
+    id = user.id,
+    name = user.name,
+    imageUrl = user.imageUrl,
+    username = user.username
+  )
+}
